1d4f9f38be7d6dea31c320f68de814444d32f22c
yuvrajSingh-rajput <sunilkumarsingh6307@gmail.com>
2025-09-27 16:38:05 +0530

    removed unnecessary files

--- src/components/msw-provider.tsx
+++
@@ -1,5 +0,0 @@
-export function MSWProvider({ children }: { children: React.ReactNode }) {
-  // Since we're using API routes instead of MSW, we don't need database initialization
-  // in the browser. The API routes handle database operations on the server side.
-  return <>{children}</>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MSWProvider } from "./components/msw-provider.tsx";
 import { ToastProvider } from "./components/ui/toast-provider.tsx";
 import "./styles/global.css";
 import App from "./App.tsx";
@@ -13,11 +12,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <MSWProvider>
           <ToastProvider>
             <App />
           </ToastProvider>
-        </MSWProvider>
     </StrictMode>
   );
 });
